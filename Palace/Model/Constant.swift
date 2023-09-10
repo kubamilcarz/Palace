4199a2da6486b4bd46976fb9ea8b46a0e4806cdc
Kuba Milcarz <kubamilcarz@yahoo.com>
2023-09-10 11:27:08 -0400

    Settings changes

--- Palace/Model/Constant.swift
+++ Palace/Model/Constant.swift
@@ -5,4 +5,11 @@
 //  Created by Kuba Milcarz on 9/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct Constant {
+    
+    static let AppVersion = "1.0"
+    
+    static let PrivacyPolicy = URL(string: "https://www.craft.me/s/OrrbBgoJ8oulQM")!
+}
--- Palace/View/SettingsView.swift
+++ Palace/View/SettingsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct SettingsView: View {
     @Environment(\.dismiss) var dismiss
@@ -13,7 +14,7 @@ struct SettingsView: View {
     var body: some View {
         NavigationStack {
             List {
-                Section("Reminders") {
+                Section {
                     Toggle(isOn: .constant(false), label: {
                         Label("Reminders", systemImage: "bell")
                     })
@@ -22,6 +23,7 @@ struct SettingsView: View {
                     }
                 }
                 .disabled(true)
+                
                 
                 Section("Data") {
                     Button {
@@ -38,22 +40,105 @@ struct SettingsView: View {
                 .disabled(true)
                 
                 Section("About") {
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Version 1.0")
-                            .font(.headline)
+                    VStack(alignment: .center, spacing: 10) {
+                        AppIcon(named: "AppIcon-Preview")
                         
-                        Link("Privacy Policy", destination: URL(string: "https://www.craft.me/s/OrrbBgoJ8oulQM")!)
+                        Text("Palace")
+                            .font(.subheadline.weight(.medium))
                         
-                        Text("© Kuba Milcarz 2023")
+                        Text("Version: \(Constant.AppVersion)")
+                            .padding(.bottom)
+                        
+                        Text("© Kuba Milcarz 2023\nAll rights reserved.")
+                        
+                        Link("Privacy Policy", destination: Constant.PrivacyPolicy)
+                            .underline()
                     }
-                    
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Resources")
-                            .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .center)
+                }
+                .listRowBackground(
+                    ZStack {
+                        RadialGradient(colors: [.accentColor.opacity(0.7), .accentColor], center: .bottomTrailing, startRadius: 100, endRadius: 300)
+                            .contrast(0.6)
+                    }
+                )
+                .font(.caption)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                
+                Section {
+                    HStack {
+                        Text("Apps")
+                            .font(.subheadline.weight(.medium))
+                        Spacer()
                         
-                        Link("Freepik", destination: URL(string: "https://www.freepik.com/free-vector/ballroom-palace-reception-hall-illustration-luxury-museum-chamber-room_3090607.htm")!)
+                        Link(destination: URL(string: "https://kubamilcarz.github.io")!) {
+                            HStack {
+                                Text("Learn more")
+                                Image(systemName: "chevron.right")
+                            }
+                        }
+                        
+                    }
+                    .listRowBackground(
+                        ZStack {
+                            RadialGradient(colors: [.blue, .orange], center: .bottomLeading, startRadius: 0, endRadius: 300)
+                            
+                            Rectangle().fill(.thickMaterial)
+                        }
+                    )
+                    
+                    HStack(spacing: 25) {
+                        Spacer(minLength: 0)
+                        
+                        AppPreview("Memorize", image: "AppIcon-Memorize", link: URL(string: "https://apps.apple.com/us/app/memorize-flashcards/id1631928972?platform=iphone")!)
+                        AppPreview("Bookie", image: "AppIcon-Bookie", link: URL(string: "https://apps.apple.com/us/app/bookie-reading-tracker/id6443825869")!)
+                        AppPreview("Coffee Note", image: "AppIcon-CoffeeNote", link: URL(string: "https://apps.apple.com/us/app/coffee-note-taste-coffee/id6447321924")!)
+                        
+                        Spacer(minLength: 0)
+                    }
+                    .padding(7)
+                    .listRowBackground(
+                        ZStack {
+                            RadialGradient(colors: [.blue, .orange], center: .bottomLeading, startRadius: 0, endRadius: 300)
+                            
+                            Rectangle().fill(.regularMaterial)
+                        }
+                    )
+                }
+                .font(.caption)
+                .foregroundStyle(.secondary)
+                
+                Section {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 5) {
+                            Text("Links: ")
+                                .font(.subheadline.weight(.medium))
+                            
+                            Link("Images from Freepik", destination: URL(string: "https://www.freepik.com/free-vector/ballroom-palace-reception-hall-illustration-luxury-museum-chamber-room_3090607.htm")!)
+                                .underline()
+                            Divider().frame(height: 10)
+                            
+                            Link("Available on Github", destination: URL(string: "https://github.com/kubamilcarz/Palace")!)
+                                .underline()
+                            
+                            #if !os(macOS)
+                            Divider().frame(height: 10)
+                            
+                            Button("Rate on App Store") {
+                                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.0) {
+                                    if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                                        SKStoreReviewController.requestReview(in: scene)
+                                    }
+                                }
+                            }
+                            #endif
+                        }
+                        .padding(.vertical, 7)
                     }
                 }
+                .font(.caption)
                 .foregroundStyle(.secondary)
             }
             .navigationTitle("Settings")
@@ -69,6 +154,26 @@ struct SettingsView: View {
                 }
             }
         }
+    }
+    
+    private func AppPreview(_ title: String, image: String, link: URL) -> some View {
+        Link(destination: link) {
+            VStack(spacing: 7) {
+                AppIcon(named: image, size: 70)
+                
+                Text(title)
+            }
+        }
+        .buttonStyle(.plain)
+    }
+    
+    private func AppIcon(named image: String, size: CGFloat = 90) -> some View {
+        Image(image)
+            .resizable()
+            .scaledToFit()
+            .frame(width: size, height: size)
+            .clipShape(RoundedRectangle(cornerRadius: size > 80 ? 18 : 15))
+            .shadow(radius: size > 80 ? 4 : 2, y: size > 80 ? 4 : 2)
     }
 }
 
